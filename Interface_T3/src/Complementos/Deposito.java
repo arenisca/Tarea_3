df7349f5b674d9056290e0b634e0495dbff92628
arenisca <vseron2000@gmail.com>
2024-11-08 23:49:42 -0300

    Actualización del depósito de productos

--- Interface_T3/src/Complementos/Deposito.java
+++ Interface_T3/src/Complementos/Deposito.java
@@ -1,29 +1,30 @@
 package Complementos;
-
 import java.util.ArrayList;
 
 /**
- * Clase genérica {@code Deposito} que representa un depósito que almacena productos
- * de cualquier tipo. Utiliza un {@code ArrayList} para gestionar los productos
- *
- * @param <T> el tipo de producto almacenado en el depósito
+ * Clase {@code Deposito} representa un depósito que almacena productos
+ * de cualquier tipo. Utiliza un {@code ArrayList} para gestionar los productos.
+ * El método {@code getProducto} entrega el primer producto o si el depósito está vacío.
+ * {@code size} indica el número de productos en el depósito.
  */
 public class Deposito<T> {
+    /**
+     * @param item el producto a agregar
+     * @param T el tipo de producto almacenado en el depósito
+     */
     private ArrayList<T> productos;
     public Deposito() {
         productos = new ArrayList<>();
     }
-
-    /**
-     * @param item el producto a agregar
-     */
     public void addProducto(T item) {
         productos.add(item);
     }
 
-    /**
-     * @return el primer producto del depósito o {@code null} si está vacío
-     */
+    /** @return Número de objetos almacenados. */
+    public int size() {
+        return productos.size();
+    }
+    /** @return el primer producto del depósito o {@code null} si está vacío */
     public T getProducto() {
         if (productos.isEmpty()) {
             return null;
