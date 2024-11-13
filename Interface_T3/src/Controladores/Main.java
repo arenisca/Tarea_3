f1f638f7834610ff33010dc356e5871bada9b2cd
arenisca <vseron2000@gmail.com>
2024-11-13 03:28:43 -0300

    Main para la perspectiva gráfica Expendedora

---
+++ Interface_T3/src/Controladores/Main.java
@@ -0,0 +1,19 @@
+package Controladores;
+import Controladores.*;
+
+/*
+* Perspectiva gráfica 2D para un sistema Comprador-Expendedor.
+* Objetivo principal: Lograr crear una interfaz gráfica para que
+* el usuario (en calidad de comprador)
+* Objetivo específico: Utilizar la tarea 1 y el proyecto de interfaz GUI
+* para lograr el objetivo principal
+
+* @author : Valentina Serón Canales
+* @version : 1.0 al 5 de noviembre, 2024
+ */
+public class Main {
+    public static void main(String[] args) {
+        Ventana_exp v = new Ventana_exp(); //única sentencia
+    }
+}
+
